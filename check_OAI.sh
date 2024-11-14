1167c29e998294fdebb429965f933c482fe09e86
takaakihoya <h.t.ikimono@gmail.com>
2024-11-14 13:22:15 +0900

    OAIの一覧取得と、IAM USERの情報を取得するスクリプトの作成

---
+++ check_OAI.sh
@@ -0,0 +1,7 @@
+#アカウント内のディストリビューションのOAIを確認し、jsonファイルに出力する
+export AWS_PROFILE="robothome-dev"
+
+aws cloudfront list-distributions \
+--query "DistributionList.Items[*].{CF_ID:Id,OAI_ID:Origins.Items[*].S3OriginConfig.OriginAccessIdentity}" \
+| jq -r '.[] | [.CF_ID, (.OAI_ID[] // "-")] | @csv' \
+> ./CloudFront-OAI.csv
---
+++ list-groups-for-user.sh
@@ -0,0 +1,14 @@
+#アカウント内のIAM USERの情報を取得し、ユーザーごとに所属しているGroupを取得する
+export AWS_PROFILE="robothome-dev"
+
+users_list=$(aws iam list-users --query "Users[].UserName" --output text)
+{
+for user in $users_list
+do
+    echo "${user} to group"
+    echo "___________________________________"
+    aws iam list-groups-for-user --user-name $user --query "Groups[].GroupName"
+    echo "___________________________________"
+    echo ""
+done
+} > output.txt
