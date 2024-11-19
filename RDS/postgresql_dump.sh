929aa476f26b0c5f39a4ad01256244370f15e271
takaakihoya <h.t.ikimono@gmail.com>
2024-11-19 09:13:25 +0900

    スクリプトを更新

--- RDS/postgresql_dump.sh
+++ RDS/postgresql_dump.sh
@@ -1,30 +1,28 @@
-bash <<'EOF'
 #!/bin/bash
 
 # 変数設定
-RDS_HOST="tateru-dev-post-02.cfhbatrcqnw3.ap-northeast-1.rds.amazonaws.com" # RDSホスト名
-RDS_USER="postgres"                                                                                                 # RDSユーザー名
-RDS_NAME="tateru-dev-post-02"                                                                                 #RDSインスタンス名
-version="PostgreSQL12.19"                                                                                          #エンジンバージョン
-BACKUP_DIR="./dev-rds/tateru-dev-post-02"                                                               # バックアップを保存するディレクトリ
-DATE=$(date +%Y%m%d)                                                                                            # 日付（YYYYMMDD形式
+date=$(date +%Y%m%d)
+rds_name="tateru-dev-post-02"
+root_user="postgres"
+endpoint="tateru-dev-post-02.cfhbatrcqnw3.ap-northeast-1.rds.amazonaws.com"
+enegine_version="PostgreSQL_12.19"
+parameter_group="default.postgres12-db-hg5ghroto5jpn2jwqiel4khuky-upgrade"
+databases_list="script/db_list_${rds_name}.txt"
+backup_path="dev-rds/${rds_name}"
+export PGPASSWORD='C4p.wVz*'
 
-# パスワードを入力せずにpg_dumpするための環境変数
-export PGPASSWORD=""
+# #dumpファイルを取得
+while read -r db;do
+    pg_dump -h $endpoint -U $root_user -d $db -f ${backup_path}/${rds_name}_${enegine_version}_${db}_dump_${date}.sql
+done < ${databases_list}
 
-# データベースのリストを取得
-databases=$(psql -h $RDS_HOST -U $RDS_USER -d postgres -t -c "SELECT datname FROM pg_database;")
+#userと、そのuserに付与された権限を作成するコマンドを取得
+# pg_dumpall -h $endpoint -U $root_user --roles-only --clean --if-exists -w -f ${backup_path}/${rds_name}_create_user_${date}.sql
 
-# 各データベースに対してバックアップを取得
-for db in $databases; do
-        BACKUP_FILE="${BACKUP_DIR}/${RDS_NAME}_${version}_${db}_dump_${DATE}.sql"
+psql -h $endpoint -U $root_user -c "SELECT * FROM pg_roles;" > ${backup_path}/${rds_name}_users_${date}.sql
 
-        # pg_dumpでバックアップを取得
-        pg_dump -h $RDS_HOST -U $RDS_USER -d $db -f "$BACKUP_FILE"
-        if [[ $? -eq 0 ]]; then
-                echo "Backup of database '$db' completed successfully."
-        else
-                echo "Backup of database '$db' failed!"
-        fi
-done
-EOF
+unset PGPASSWORD
+
+aws rds describe-db-parameters --db-parameter-group-name ${parameter_group} > ${backup_path}/${rds_name}_${parameter_group}_${enegine_version}_${date}.csv
+
+
