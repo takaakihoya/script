57277e5377bfb5e9b4db832ea5c480ec9bf0ef6d
takaakihoya <h.t.ikimono@gmail.com>
2024-11-29 17:19:49 +0900

    変数を変更

--- ecr/image_pull_and_push.sh
+++ ecr/image_pull_and_push.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
+#前提として .aws/config ファイルに各プロファイル情報を記載する必要がああります
 
 #アーカイブ元アカウント情報
-SRC_ACCOUNT_ID="396609837941"
-SRC_REGION="us-east-1"
+SRC_ACCOUNT_ID="637423579101"
+SRC_REGION="ap-northeast-1"
 SRC_REPO_IMAGE_LIST_FILE="./dev_classic_rh_repo_image_list.txt"
-SOURCE_PROFILE="robothome-dev"
+SOURCE_PROFILE="sand-hoya"
 
 #アーカイブ先アカウント情報
-DEST_ACCOUNT_ID="879562317725"
+DEST_ACCOUNT_ID="840471652923"
 DEST_REGION="ap-northeast-1"
 DEST_REPO_LIST_FILE="./log_archive_repo_list.txt"
-DEST_PROFILE="log-archive-ecr"
+DEST_PROFILE="sand-fushimi"
 
 #アーカイブするイメージタグにつける日付
 TODAY=$(date +%Y%m%d)
 
 #アーカイブ元アカウントにログイン
-#今回はアーカイブ元がバージニアなのでregion指定する
-login_to_ecr_a() {
+login_to_src_ecr() {
   aws sso login --profile $SOURCE_PROFILE
-  aws ecr get-login-password --profile $SOURCE_PROFILE --region $SRC_REGION | docker login --username AWS --password-stdin $SRC_ACCOUNT_ID.dkr.ecr.$SRC_REGION.amazonaws.com
+  aws ecr get-login-password --profile $SOURCE_PROFILE --region | docker login --username AWS --password-stdin $SRC_ACCOUNT_ID.dkr.ecr.$SRC_REGION.amazonaws.com
 }
 
 #アーカイブ先アカウントにログイン
-login_to_ecr_b() {
+login_to_dest_ecr() {
   aws sso login --profile $DEST_PROFILE
   aws ecr get-login-password --profile $DEST_PROFILE | docker login --username AWS --password-stdin $DEST_ACCOUNT_ID.dkr.ecr.$DEST_REGION.amazonaws.com
 }
@@ -31,7 +31,7 @@ login_to_ecr_b() {
 #アーカイブ元からはリポジトリと最新のimageID、アーカイブ先からはリポジトリを読み込んで変数に入れる
 paste -d, "$SRC_REPO_IMAGE_LIST_FILE" "$DEST_REPO_LIST_FILE" | while IFS=, read -r src_repo src_image_id dest_repo
 do
-  login_to_ecr_a
+  login_to_src_ecr
   #pullするイメージのURI
   SOURCE_IMAGE_URI="${SRC_ACCOUNT_ID}.dkr.ecr.${SRC_REGION}.amazonaws.com/${src_repo}:${src_image_id}"
   
@@ -43,6 +43,6 @@ do
   docker tag $SOURCE_IMAGE_URI $DEST_IMAGE_URI
 
 #アーカイブ先アカウントにイメージをプッシュ
-  login_to_ecr_b
+  login_to_dest_ecr
   docker push $DEST_IMAGE_URI
 done
